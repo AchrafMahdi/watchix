356db70834857c224f0b953d907016d4035651f6
Achraf <achraf@example.com>
2024-08-14 04:14:37 +0100

    Modified paginationControls for it to work with any page

--- src/components/pagination/PaginationControls.jsx
+++ src/components/pagination/PaginationControls.jsx
@@ -6,21 +6,31 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 
 const PaginationControls = ({ totalPages }) => {
   const searchParams = useSearchParams();
+  const path = usePathname();
+
   const page = Number(searchParams.get("page") || 1);
+  const params = new URLSearchParams(searchParams);
 
   const isFirstPage = page <= 1;
   const isLastPage = page >= totalPages;
+
+  // Prepare URLs for previous and next pages while preserving other params
+  if (!isFirstPage) params.set("page", page - 1);
+  const prevHref = `${path}?${params.toString()}`;
+
+  params.set("page", page + 1);
+  const nextHref = `${path}?${params.toString()}`;
 
   return (
     <Pagination>
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious
-            href={`?page=${page - 1}`}
+            href={prevHref}
             className={isFirstPage ? "pointer-events-none opacity-50" : ""}
             aria-disabled={isFirstPage}
           />
@@ -28,7 +38,7 @@ const PaginationControls = ({ totalPages }) => {
         {page + " / " + totalPages}
         <PaginationItem>
           <PaginationNext
-            href={`?page=${page + 1}`}
+            href={nextHref}
             className={isLastPage ? "pointer-events-none opacity-50" : ""}
             aria-disabled={isLastPage}
           />
